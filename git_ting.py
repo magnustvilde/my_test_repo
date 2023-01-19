044b6721a059444d7468c53a4d87315f02b108b0
Juni E. J <jujoa007@osloskolen.no>
2023-01-19 13:21:30 +0100

    endring i git_ting.py

--- git_ting.py
+++ git_ting.py
@@ -1,6 +1,11 @@
-print('git fetch')
-print('git add .')
-print('git commit -m \"Min melding!\"')
-print('git push')
+print('ENDRINGER?'
+'\n git status')
 
-print('git pull')
+print('\n\n HENT TING'
+'\n git fetch'
+'\n git pull')
+
+print('\n\n SEND TING'
+'\n git add .'
+'\n git commit -m \"Min melding!\"'
+'\n git push')
