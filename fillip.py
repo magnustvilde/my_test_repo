139c56cb2ebc9959c629a04e31d075bd96a762ad
Smagga <sverremagnus@icloud.com>
2023-01-24 14:22:58 +0100

    Auto stash before merge of "main" and "origin/main"

---
+++ fillip.py
@@ -0,0 +1,11 @@
+print("Fillip er dum")
+print("sheesh") 
+a, b = input("er filip ikke smart? "), input("er filip wack? ")
+if b == ("ja"):
+    print("da like smagga deg:)")
+else:
+    print("smagga hater deg")
+if a == ("ja"):
+    print("ingen liker deg")
+else:
+    print("smagga liker deg")
