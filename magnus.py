988f1f1aa87241640594f7035078f3f666ea829f
Jun1l1ll1 <118979783+Jun1l1ll1@users.noreply.github.com>
2023-03-24 12:42:15 +0100

    Update magnus.py

--- magnus.py
+++ magnus.py
@@ -1,7 +1,7 @@
 # code in main
 # no code here
 
-[print('Hello') for i in range(5)]
+[print('Die') for i in range(5)]
 
 # trying to create a merge conflict in the same branch
 x = lambda y, z : y>z
